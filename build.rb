7ee5e5ec3733287b9eb5453a4373149ad241128a
David Francisco <dmfrancisc@gmail.com>
2012-08-19 18:50:33 +0100

    Copy Less and CoffeeScript sources after build script

---
+++ build.rb
@@ -0,0 +1,20 @@
+# Run Middleman build script
+
+system "bundle exec middleman build"
+
+# Copy CSS (Less) and JavaScript (CoffeeScript) source files to the "build" directory
+# Read "http://blog.dmfranc.com/post/29475625982/developer-friendly-websites" for more information
+
+require "fileutils"
+
+source_dir = 'source'
+css_dir = 'css'
+js_dir  = 'js'
+
+files =  Dir.glob(File.join(source_dir, css_dir, "**/*.less"))
+files << File.join(source_dir, js_dir, 'application.coffee')
+
+files.each do |file|
+  puts "Copying '#{file.gsub('source/', 'build/')}'..."
+  FileUtils.cp(file, file.gsub('source/', 'build/'))
+end
