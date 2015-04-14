1747b4d0cf14cf677847cebb9e11348825892199
David Francisco <dmfrancisc@gmail.com>
2015-04-14 13:41:43 +0100

    Starting fresh — A reboot for the next 5 years

--- config.rb
+++ config.rb
@@ -1,86 +1,17 @@
-###
-# Page options, layouts, aliases and proxies
-###
+set :css_dir, "css"
+set :js_dir, "js"
+set :images_dir, "img"
 
-# Per-page layout changes:
-#
-# With no layout
-# page "/path/to/file.html", :layout => false
-#
-# With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
-#
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
-
-# Proxy (fake) files
-# page "/this-page-has-no-template.html", :proxy => "/template-file.html" do
-#   @which_fake_page = "Rendering a fake page with a variable"
-# end
-
-###
-# Helpers
-###
-
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
-
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
-
-set :css_dir, 'css'
-set :js_dir, 'js'
-set :images_dir, 'img'
-
-# Build-specific configuration
-configure :build do
-  # For example, change the Compass output style for deployment
-  activate :minify_css
-
-  # Minify Javascript on build
-  activate :minify_javascript
-
-  # Enable cache buster
-  # activate :cache_buster
-  activate :asset_hash
-
-  # Use relative URLs
-  activate :relative_assets
-
-  # Compress PNGs after build
-  # First: gem install middleman-smusher
-  # require "middleman-smusher"
-  # activate :smusher
-  #
-  # I'm doing this manually, since smusher removes the bKGD chunk, which defines
-  # the fallback color for browsers that don't support the alpha channel (IE6)
-
-  # Or use a different image path
-  # set :http_path, "/Content/images/"
-
-  # Gzip text files
-  activate :gzip
-  # See https://gist.github.com/2200790
-  # and http://guides.rubyonrails.org/v3.1.0/asset_pipeline.html#server-configuration
-
-  # Pretty URLs (Directory Indexes)
-  activate :directory_indexes
-  page "/404.html", :directory_index => false
-  page "/500.html", :directory_index => false
+# Environment configuration
+configure :development do
+  activate :livereload
 end
 
-after_configuration do
-  # Add CSS directory to the Less compiler
-  # Less.paths << File.join(source_dir, css_dir)
-
-  # Add all subdirectories inside the CSS folder to the Less compiler
-  Dir.glob(File.join(source_dir, css_dir, "**/")).each do |dir|
-    Less.paths << File.expand_path(dir)
-  end
+# Build-specific configs
+configure :build do
+  activate :minify_css
+  activate :minify_javascript
+  activate :asset_hash
+  activate :relative_assets
+  activate :directory_indexes
 end
