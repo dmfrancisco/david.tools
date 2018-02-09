2a32abbc6aff2ebf7cd6acaa2abfd666f62543e8
David Francisco <dmfrancisc@gmail.com>
2018-02-09 21:43:32 +0000

    Minor changes to react-snap config for server-only

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
 import React from "react";
-import { hydrate, render } from "react-dom";
+import { render } from "react-dom";
 import "normalize.css";
 import "typeface-karla";
 
 import App from "./App";
 import "./index.css";
 
-const rootElement = document.getElementById("root");
-
-if (rootElement.hasChildNodes()) {
-  hydrate(<App />, rootElement);
-} else {
-  render(<App />, rootElement);
-}
+render(<App />, document.getElementById("root"));
