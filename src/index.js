8b7b7186a490f42a963a1765184d1da6f7730090
David Francisco <dmfrancisc@gmail.com>
2018-02-09 01:00:07 +0000

    Initial setup for react-snap

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { hydrate, render } from "react-dom";
 import "normalize.css";
 import "typeface-karla";
 
 import App from "./App";
 import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const rootElement = document.getElementById("root");
+
+if (rootElement.hasChildNodes()) {
+  hydrate(<App />, rootElement);
+} else {
+  render(<App />, rootElement);
+}
