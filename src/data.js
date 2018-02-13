5f3adec3cc76223ad9d5122df55d4f0644204957
David Francisco <dmfrancisc@gmail.com>
2018-02-13 22:10:17 +0000

    Minor language change

--- src/data.js
+++ src/data.js
@@ -33,7 +33,7 @@ export default {
   title:
     "Web developer from Coimbra, Portugal working remotely at Union. Currently focused on the next version of our web application.",
   description:
-    "My name is David Francisco. I have been working as a full-stack developer since 2012. My primary focus is the front-end and design of the software platform we are building. Most of my work is not publicly available, but I build open source side-projects whenever possible. The following is a list of my favorites.",
+    "My name is David Francisco. I have been working as a full-stack developer since 2012. My primary focus is the front-end and design of the software platform we are building. Most of my work is not publicly available, but I create open source side-projects whenever possible. The following is a list of my favorites.",
   nav: [
     {
       text: "Blog",
